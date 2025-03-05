d4f0be42bb6dd63d7934cff5ba9b63b503854a90
99789studentmerito <wrx99789@student.wroclaw.merito.pl>
2025-03-05 22:10:12 +0100

    changed port

--- src/main/java/pl/wsb/FitnessTracker.java
+++ src/main/java/pl/wsb/FitnessTracker.java
@@ -1,13 +1,22 @@
 package pl.wsb;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class FitnessTracker {
 
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer() {
+        return factory -> factory.setPort(9090);
+    }
     public static void main(String[] args) {
         SpringApplication.run(FitnessTracker.class, args);
     }
 
+
 }
+
